a9b5da9eb74b844c614edbf2c2c155d53cb47cd1
Jonathan Nyberg <jona_nyberg@hotmail.com>
2013-12-15 12:48:52 +0100

    Added localhost database connection
    Fixat till databaskoppling localhost

--- index.js
+++ index.js
@@ -2,13 +2,17 @@
 
 
 var kraken = require('kraken-js'),
+   
+    db = require('./lib/database'),
     // Variabel form middleware
     millionsServed = require('./lib/millionsServed'),
+    
     app = {};
 
 //app.configure gets its data from ./configuration/app.json if dev and prod is wanted. Create another app-development.json and set the ENV variables accordingly
 app.configure = function configure(nconf, next) {
     // Fired when an app configures itself
+    db.config(nconf.get('databaseConfig'));
     next(null);
 };
 
